ff47800584c7dfc0cfd9b85f8d9dd3f73ad4ee5b
ayushyadav468 <ayushyadav468@gmail.com>
2018-03-03 13:16:40 +0530

    Initial Commmit

--- app/src/main/java/com/example/ayushyadav/intentandalarms/MainActivity.java
+++ app/src/main/java/com/example/ayushyadav/intentandalarms/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.ayushyadav.intentandalarms;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +21,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+
     public void call(View view){
 
-        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
+        int permissionGranted = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
 
+        if(permissionGranted == PackageManager.PERMISSION_GRANTED){
+            callUs();
+        }else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CALL_PHONE}, 1);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 1){
+            int grantResult = grantResults[0];
+            if(grantResult == PackageManager.PERMISSION_GRANTED){
+                callUs();;
+            }else {
+                Toast.makeText(this,"Permission Denied. ",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     private void callUs(){
@@ -28,6 +56,6 @@ public class MainActivity extends AppCompatActivity {
         shareIntent.setAction(Intent.ACTION_CALL);
         shareIntent.setData(Uri.parse("tel:+918447410671"));
         startActivity(shareIntent);
-
     }
+
 }
