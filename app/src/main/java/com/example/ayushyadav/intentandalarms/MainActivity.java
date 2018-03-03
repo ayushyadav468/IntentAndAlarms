13652dba41fdc23ee9cd3bc2701378cb86cca791
Ayush Yadav <ayushyadav@Ayushs-MacBook-Air.local>
2018-03-03 12:25:21 +0530

    Initial Commmit

---
+++ app/src/main/java/com/example/ayushyadav/intentandalarms/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.ayushyadav.intentandalarms;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void call(View view){
+
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
+
+    }
+
+    private void callUs(){
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_CALL);
+
+    }
+}
